597892bad8a8a336ebf97c78055e3111fa3186ee
loicFix <loic.warisse@epitech.eu>
2020-03-27 09:37:41 +0100

    [MINOR] Decryptage

--- src/main.c
+++ src/main.c
@@ -12,14 +12,18 @@ char *str_infile(char *path)
     char *buff = malloc(sizeof(char) * 10000);
     int fd = open(path, O_RDONLY);
 
+    if (fd == -1)
+      exit (84);
     read(fd, buff, 10000);
-    printf("%s\n", buff);
+
     return (buff);
 }
 
 int main(int ac, char **av)
 {
     char *str;
+    int nbr = (atoi(av[2]) < 26 ? atoi(av[2]) : atoi(av[2]) % 26);
+ 
     if (ac != 3)
         return (84);
     str = str_infile(av[1]);
