f48f44d3c06303e93a56818df926fbf78f843f3e
loic.warisse@epitech.eu <loic.warisse@epitech.eu>
2020-03-27 09:18:04 +0100

    [MINOR] Begin main

--- src/main.c
+++ src/main.c
@@ -7,7 +7,21 @@
 
 #include "../include/header.h"
 
+char *str_infile(char *path)
+{
+    char *buff = malloc(sizeof(char) * 10000);
+    int fd = open(path, O_RDONLY);
+
+    read(fd, buff, 10000);
+    printf("%s\n", buff);
+    return (buff);
+}
+
 int main(int ac, char **av)
 {
+    char *str;
+    if (ac != 3)
+        return (84);
+    str = str_infile(av[1]);
     return (0);
 }
