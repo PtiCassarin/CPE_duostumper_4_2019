72339fcd84bf2505b184269c861b835206c4d264
Jimmy Cassarino <jimmy.cassarino@epitech.eu>
2020-04-10 11:08:29 +0200

    [Feat][Jimmy]: Change make_pasta.c

--- src/make_pasta.c
+++ src/make_pasta.c
@@ -9,5 +9,34 @@
 
 void make_pasta(instance_t *inst, char *buff_user)
 {
-    
+    inst->tomato -= 5;
+    inst->pasta -= 2;
+    inst->olive -= 6;
+    inst->cheese -= 3;
+    inst->ham -= 4;
+    printf("REMOVE\n");   
+}
+
+void check_frigo(instance_t *inst, char *buff_user)
+{
+    if (inst->tomato < 5) {
+        printf("'make pasta': not enough tomato");
+        return;
+    }
+    if (inst->pasta < 2) {
+        printf("'make pasta': not enough pasta");
+        return;
+    }
+    if (inst->olive < 6) {
+        printf("'make pasta': not enough olive");
+        return;
+    }
+    if (inst->cheese < 3) {
+	printf("'make pasta': not enough cheese");
+	return;
+    }
+    if (inst->ham < 4) {
+	printf("'make pasta': not enough ham");
+	return;
+    make_pasta(inst, buff_user);
 }
