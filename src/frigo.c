752ff5c991691020194eb6fa3312ffef38b86969
Jimmy Cassarino <jimmy.cassarino@epitech.eu>
2020-04-10 10:43:04 +0200

    [Feat][Jimmy]: Create frigo.c

---
+++ src/frigo.c
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2020
+** fridge
+** File description:
+** frigo.c
+*/
+
+#include "../include/header.h"
+
+void frigo(instance_t *inst)
+{
+    printf("Tomato = %d\n", inst->tomato);
+    printf("Dough= %d\n", inst->dought);
+    printf("Onion= %d\n", inst->onion);
+    printf("Pasta= %d\n", inst->pasta);
+    printf("Olive= %d\n", inst->olive);
+    printf("Pepper= %d\n", inst->pepper);
+    printf("Ham= %d\n", inst->ham);
+    printf("Cheese= %d\n", inst->cheese);
+}
